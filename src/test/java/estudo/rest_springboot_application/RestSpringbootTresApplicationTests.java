23a9576972c064c5a6d18fee92ab63f395069400
Cassius José de Souza Barbosa <cassius.barbosa@wdev.int>
2024-08-26 17:12:12 -0300

    Commit inicial

---
+++ src/test/java/estudo/rest_springboot_application/RestSpringbootTresApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.rest_springboot_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootTresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
