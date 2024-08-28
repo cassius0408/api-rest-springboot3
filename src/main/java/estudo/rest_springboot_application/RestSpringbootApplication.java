30703f85ce5ef0a5541ff955468d2f64cf681349
cassius.barbosa <cassius.barbosa@gmail.com>
2024-08-28 18:06:39 -0300

    Bugfix CORS.

--- src/main/java/estudo/rest_springboot_application/RestSpringbootApplication.java
+++ src/main/java/estudo/rest_springboot_application/RestSpringbootApplication.java
@@ -1,8 +1,11 @@
 package estudo.rest_springboot_application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Defaut Server URL")})
 @SpringBootApplication
 public class RestSpringbootApplication {
 
